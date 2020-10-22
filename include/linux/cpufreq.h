ca5d953a3806d8b1bd19eb65504f2f9f153c6365
rmnscnce <rmnscnce@ya.ru>
2020-10-22 14:45:44 +0700

    Linux 3.18.140 for PLE kernel-plus v1.2.1
    Changes:
    - Switched default CPU governor back to 'darkness'
    - Removed 'lionfish' because of reboots when the battery is 35% or lower

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -531,9 +531,6 @@ extern struct cpufreq_governor cpufreq_gov_alucard;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
 extern struct cpufreq_governor cpufreq_gov_bioshock;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_bioshock)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONFISH)
-extern struct cpufreq_governor cpufreq_gov_lionfish;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lionfish)
 #endif
 
 /*********************************************************************
